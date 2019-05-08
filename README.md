e1dd0a8fdad6ebfce80ce2f76304b37568ec6cca
Atsushi Hamada <a.hamada.biz@gmail.com>
2019-05-08 17:18:39 +0900

    Change project name for heroku

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SharePF
+# share-pf
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.3.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SharePF'`, () => {
+  it(`should have as title 'share-pf'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('SharePF');
+    expect(app.title).toEqual('share-pf');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to SharePF!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to share-pf!');
   });
 });
